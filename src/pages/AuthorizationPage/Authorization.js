68b38c5195b25331729b34f5643167bb68e5a009
Julia Mekto <mektojulia1@gmail.com>
2019-03-05 13:29:08 +0300

    added authorization form

---
+++ src/pages/AuthorizationPage/Authorization.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Authorization.css';
+import AuthorizationForm from '../../components/AuthorizationForm.jsx';
+
+class Authorization extends Component {
+  render() {
+    return (
+      <AuthorizationForm />
+    );
+  }
+}
+
+export default Authorization;
