5a047156bae605e6080da44a5cbb229f438c09dd
Julia Mekto <mektojulia1@gmail.com>
2019-03-06 13:23:09 +0300

    adding authorization form validation

---
+++ src/components/InlineError.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class InlineError extends Component {
+  render(text) {
+    return (
+       <p>{text}</p>
+    );
+  }
+}
+
+export default InlineError;
--- src/components/authorizationForm/AuthorizationForm.jsx
+++ src/components/authorizationForm/AuthorizationForm.jsx
@@ -5,19 +5,36 @@ import Button from '../Button.jsx';
 
 
 class AuthorizationForm extends Component {
-  render() {
-    return (
-      <div className="authorization-form-wrapper">
-        <h2>Sign in</h2>
-        <form className="authorization-form">
-            <FormInput name="text" type="email" placeholder="email"/>
-            <FormInput name="password" type="password" placeholder="password"/>
-            <Button type="submit" caption="sign in"/>
-        </form>
-        <a href="#">or sign up</a>
-      </div>
-    );
+  constructor (props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: '',
+      formErrors: {email: '', password: ''},
+      emailValid: false,
+      passwordValid: false,
+      formValid: false
+    }
   }
+  handleUserInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value});
+  }
+ render() {
+  
+   return (
+     <div className="authorization-form-wrapper">
+       <h2>Sign in{this.state.test}</h2>
+       <form className="authorization-form">
+           <FormInput id="email" name="email" type="email" placeholder="email"  value={this.state.email} onChange={this.handleUserInput}/>
+           <FormInput id="password" name="password" type="password" placeholder="password" value={this.state.password} onChange={this.handleUserInput}/> 
+           <Button type="submit" caption="sign in"/>
+       </form>
+       <a href="#">or sign up</a>
+     </div>
+   );
+ }
 }
 
 export default AuthorizationForm;
