569e889f439ee3702b190bd48103c5a2589693d0
Julia Mekto <mektojulia1@gmail.com>
2019-03-13 13:08:48 +0300

    added UI form fixes,a success registration modal

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -6,7 +6,7 @@ import './Intro.css';
 const Intro = () => {
     return (
       <section className="intro-wrapper">
-          <h1 className="intro-wrapper__title">Welcome to a plane ticket resevation system!</h1>
+          <h1 className="intro-wrapper__title">Welcome to a plane ticket reservation system</h1>
           <Link to="/authorization"> 
                 <Button type='button' 
                         caption="Let's start"
--- src/components/registrationForm/RegistrationForm.jsx
+++ src/components/registrationForm/RegistrationForm.jsx
@@ -1,45 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import FormInput from '../FormInput.jsx';
 import Button from '../Button.jsx';
+import Modal from '../modal/Modal.jsx';
 import './RegistrationForm.css';
 
 
-const RegistrationForm = () => {
-   return (
-     <div className="registration-form-wrapper">
-       <h2 className="registration-form-wrapper__title">Sign Up</h2>
-       <form className="registration-form">
-           <FormInput id="password" 
-                      name="password" 
-                      type="password" 
-                      placeholder="full name"/> 
-           <FormInput id="password" 
-                      name="password" 
-                      type="password"
-                      placeholder="username"/> 
-           <FormInput id="email" 
-                      name="email" 
-                      type="email" 
-                      placeholder="email" />
-           <FormInput id="password" 
-                      name="password" 
-                      type="password" 
-                      placeholder="password"/> 
-           <FormInput id="password" 
-                      name="password" 
-                      type="password" 
-                      placeholder="repeat password"/> 
-           <Button type="submit" 
-                   caption="sign up"/>
-       </form>
-       <div>
-       <FormInput type="checkbox"/> 
-       <span>I agree to the terms and conditions</span>
-       </div>
-       <Link to="/authorization" className="form-link"> or sign in </Link>
-     </div>
-   );
+class RegistrationForm extends Component {
+    constructor (props) {
+      super(props);
+      this.state = {
+        modal: {
+          show: false
+        }
+      }
+    }
+
+  showModal = (e) => {
+      e.preventDefault();
+      this.setState ({ modal : {show: true}});
+  }
+
+  hideModal = () => {
+      this.setState ({modal : {show: false}});
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+         <div className="registration-form-wrapper">
+           <h2 className="registration-form-wrapper__title">Sign Up</h2>
+           <form className="registration-form">
+               <FormInput id="password" 
+                           name="password" 
+                           type="password" 
+                           placeholder="full name"/> 
+               <FormInput id="password" 
+                           name="password" 
+                           type="password"
+                           placeholder="username"/> 
+               <FormInput id="email" 
+                           name="email" 
+                           type="email" 
+                           placeholder="email" />
+               <FormInput id="password" 
+                           name="password" 
+                           type="password" 
+                           placeholder="password"/> 
+               <FormInput id="password" 
+                           name="password" 
+                           type="password" 
+                           placeholder="repeat password"/> 
+               <Button type="submit" 
+                       caption="sign up"
+                       action={this.showModal}/>
+           </form>
+           <div className="privacy-policy-wrapper">
+               <label className="container">
+                 <FormInput type="checkbox" /> 
+                 <span className="checkmark"></span>
+               </label>
+               <span className="caption">I agree to the terms and conditions</span>
+           </div>
+           <Link to="/authorization" className="form-link"> i already have an account </Link>
+         </div>
+         <Modal show={this.state.modal.show} handleClose={this.hideModal}>
+          <div className="greeting">
+              <span className="greeting__title">Great!</span>
+              <span className="greeting__text">You've been successfully registered!</span>
+          </div>
+          <div className="img-wrapper"></div>
+          <Link to="/" className="route_link"><Button action={this.hideModal} caption="start"/></Link>
+        </Modal>
+      </React.Fragment>
+    );
+  }
 }
 
 export default RegistrationForm;
